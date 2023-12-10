61641029f97df27a6cb7478e824eaae23b58b3c6
william-rauwens-oliver <william@PcWIlly>
2023-12-10 11:02:43 +0100

    add code

--- ia.py
+++ ia.py
@@ -1,24 +1,25 @@
 import random
 
-def ia(board, player, difficulty):
+def ia(board, signe, difficulty):
     flat_board = [element for row in board for element in row]
 
     if difficulty == "facile":
         return jouer_aleatoire(flat_board)
     elif difficulty == "moyen":
-        return jouer_moyen(flat_board, player)
+        return jouer_moyen(flat_board, signe)
     elif difficulty == "difficile":
-        return jouer_difficile(flat_board, player)
+        return jouer_difficile(flat_board, signe)
     else:
         raise ValueError("Niveau de difficulté non valide")
 
 def jouer_aleatoire(board):
-    # Jouer un coup aléatoire
+    # Joue un coup aléatoire
     empty_cells = [i for i, cell in enumerate(board) if cell == 0]
     return random.choice(empty_cells) if empty_cells else None
 
-def jouer_moyen(board, player):
+def jouer_moyen(board, signe):
     # Si l'IA peut gagner, l'IA joue pour gagner
+    player = 1 if signe == 'X' else 2
     for i in range(9):
         if board[i] == 0:
             board[i] = player
@@ -27,7 +28,7 @@ def jouer_moyen(board, player):
             board[i] = 0
 
     # Si l'adversaire peut gagner, l'IA joue pour bloquer
-    opponent = -player
+    opponent = 2 if player == 1 else 1
     for i in range(9):
         if board[i] == 0:
             board[i] = opponent
@@ -38,8 +39,9 @@ def jouer_moyen(board, player):
 
     return jouer_aleatoire(board)
 
-def jouer_difficile(board, player):
+def jouer_difficile(board, signe):
     # L'IA difficile joue pour gagner à chaque fois
+    player = 1 if signe == 'X' else 2
     for i in range(9):
         if board[i] == 0:
             board[i] = player
@@ -48,7 +50,7 @@ def jouer_difficile(board, player):
             board[i] = 0
 
     # Si l'adversaire peut gagner, l'IA joue pour bloquer
-    opponent = -player
+    opponent = 2 if player == 1 else 1
     for i in range(9):
         if board[i] == 0:
             board[i] = opponent
